373908ea74096879ff513dbeac9014352a373e65
Ankit Kumar <146810887+Kumar22Ankit@users.noreply.github.com>
2024-12-05 11:56:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ region = "ap-south-1"
 resource "aws_instance" "prod_instance01" {
 ami = "ami-053b12d3152c0cc71"
 instance_type = "t2.nano"
-subnet_id = "0befd88457a8ccab3"
+subnet_id = "subnet-070393be58e4eb991"
 tags = {
   Name = "webserver"
 }
