8684cd2ede740be1afd45d7c778064222bbc91af
Ankit Kumar <146810887+Kumar22Ankit@users.noreply.github.com>
2024-12-05 11:49:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
-resource "aws_instance" "prod_instance01" {
+provider "aws" {
 region = "ap-south-1"
+}
+resource "aws_instance" "prod_instance01" {
 ami = "ami-053b12d3152c0cc71"
 instance_type = "t2.nano"
 subnet_id = "0befd88457a8ccab3"
